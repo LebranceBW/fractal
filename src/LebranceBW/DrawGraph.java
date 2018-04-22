c127be5d9e75b4b085728672a5b00d16ee7e8f49
Lebrance <LebranceBW@foxmail.com>
2018-04-22 19:28:20 +0800

    添加自定义标题栏的拖动功能

--- src/LebranceBW/DrawGraph.java
+++ src/LebranceBW/DrawGraph.java
@@ -2,13 +2,13 @@ package LebranceBW;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class DrawGraph extends Application 
 {
+	private double xbias, ybias; // 按钮按下时相对窗体的偏移
 	public static void main(String[] args)
 	{
 		DrawGraph.launch(args);
@@ -17,12 +17,19 @@ public class DrawGraph extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-		primaryStage.setScene(new Scene(root));
+		Scene root = new Scene(FXMLLoader.load(getClass().getResource("main.fxml")));
+		root.getRoot().setOnMouseDragged(e->{
+			primaryStage.setX(e.getScreenX() - xbias);
+			primaryStage.setY(e.getScreenY() - ybias);
+		});
+		root.getRoot().setOnMousePressed(e->{
+			xbias = e.getScreenX() - primaryStage.getX();
+			ybias = e.getScreenY() - primaryStage.getY();
+
+		});
+		primaryStage.setScene(root);
 		primaryStage.initStyle(StageStyle.UNDECORATED);
 		primaryStage.show();
-		
-		
 	}
 	
 }
