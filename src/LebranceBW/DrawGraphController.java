5a03e77fb5f18e08dd4aeae8f2a701f8e6d135e5
Lebrance <LebranceBW@foxmail.com>
2018-04-22 13:30:28 +0800

    记录笔记 ✏

--- src/LebranceBW/DrawGraphController.java
+++ src/LebranceBW/DrawGraphController.java
@@ -5,26 +5,23 @@ import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class DrawGraphController implements Initializable
 {
+	// 定义在FXML中的组件Controller
 	@FXML private Canvas dst_canvas;
 	@FXML private Button sure_btn, quit_btn;
 	@FXML private Slider slider;
-//	@FXML private VBox root;
-//	@FXML private BorderPane title_panel;
+	@FXML private Label counter_label;
 	
 	// 蕨类叶子的绘制矩阵，概率自上到下为1% 7% 7% 85%
 	private final double[][] coefficient = 
@@ -35,16 +32,17 @@ public class DrawGraphController implements Initializable
 		{0.85, 0.04, 0, -0.04, 0.85, 1.6},
 	};
 	
+	// 初始化，绑定监视器
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
 		drawgraph(dst_canvas, (int)slider.getValue());
 		quit_btn.setOnMouseClicked(e->{Platform.exit();});
-		slider.valueProperty().addListener(e->drawgraph(dst_canvas, (int)slider.getValue()));
-//		title_panel.setOnMouseDragged(e->{
-//			root.getParent().setLayoutX(e.getSceneX());
-//			root.getParent().setLayoutY(e.getSceneY());
-//		});
+		slider.valueProperty().addListener(e->
+		{
+			drawgraph(dst_canvas, (int)slider.getValue());
+			counter_label.setText(String.format("循环：%.1fW次", slider.getValue()/10000));
+		});
 	}
 	
 	private void drawgraph(Canvas canvas, int counts)
@@ -80,7 +78,7 @@ public class DrawGraphController implements Initializable
 			x0 = x1;
 			y0 = y1;
 			
-			// 设定绘图边界
+			// 绘图坐标转换
 			// x -5 ~ 5
 			// y 0 - 10
 			int draw_x = (int) ((width / 2) + x1/10*(width-1) );
@@ -88,7 +86,7 @@ public class DrawGraphController implements Initializable
 			Color colorRand[] = {Color.RED, Color.TURQUOISE, Color.BLUE, Color.GREEN};
 			pw.setColor(draw_x, draw_y, colorRand[index]);
 		}
-		// 清除上次的内容然后重新绘制
+		// 清除上次的内容然后将Image回执到Canvas上
 		canvas.getGraphicsContext2D().clearRect(0, 0, width, height);
 		canvas.getGraphicsContext2D().drawImage(wImage, 0, 0);
 	}
